a957fecedbd1f08e63043e46f09c3ddcd515374c
kx47g <67031874+kx47g@users.noreply.github.com>
2020-07-15 11:44:46 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,20 @@ let 移動距離 = 0
 let 方角 = 0
 input.onButtonPressed(Button.A, function () {
     移動距離 = 0
-    方角 = input.compassHeading()
     while (移動距離 < 31) {
         if (方角 - 10 <= input.compassHeading() && input.compassHeading() <= 方角 + 10) {
             plenbit.soccerMotion(plenbit.SocMotions.LongDribble)
-            移動距離 += 5
+            plenbit.soccerMotion(plenbit.SocMotions.LongDribble)
+            plenbit.soccerMotion(plenbit.SocMotions.LongDribble)
+            移動距離 += 3
         } else if (方角 + 10 < input.compassHeading() && input.compassHeading() < 方角 + 170) {
             plenbit.danceMotion(plenbit.DanceMotions.DanceLStep)
         } else if (方角 - 10 > input.compassHeading() && input.compassHeading() < 方角 - 170) {
             plenbit.danceMotion(plenbit.DanceMotions.DanceRStep)
         }
     }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.compassHeading())
+    方角 = input.compassHeading()
 })
