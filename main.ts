9f06e98ccf4af3838f9b28182e4693a082e6333f
kx47g <67031874+kx47g@users.noreply.github.com>
2020-07-15 09:53:22 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-plenbit.soccerMotion(plenbit.SocMotions.DefenseLStep)
+plenbit.eyeLed(plenbit.LedOnOff.On)
+plenbit.danceMotion(plenbit.DanceMotions.DanceBow)
+plenbit.stdMotion(plenbit.StdMotions.WalkForward)
+plenbit.stdMotion(plenbit.StdMotions.WalkForward)
+plenbit.danceMotion(plenbit.DanceMotions.TwistDance)
+plenbit.stdMotion(plenbit.StdMotions.WalkBack)
+plenbit.stdMotion(plenbit.StdMotions.WalkBack)
 basic.forever(function () {
 	
 })
